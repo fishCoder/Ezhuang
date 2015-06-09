fafacbd909a1ef41685643e830b5694e3675ae98
flb1019 <flb1019@gmail.com>
2015-06-09 14:27:14 +0800

    分享方案

---
+++ app/src/main/java/com/ezhuang/model/Scheme.java
@@ -0,0 +1,21 @@
+package com.ezhuang.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2015/6/8 0008.
+ */
+public class Scheme implements Serializable {
+    public String styleName;
+    public String styleId;
+    public String createTime;
+    public String cover;
+    public String schemeId;
+    public float  price;
+    public int    schemeType;
+    public int    state;
+    public String apartmentLayout;
+    public String providerId;
+    public String schemeName;
+    public String introduction;
+}
